d54934644382f57b76bc5235fc9818b3b134cf77
Fangchen Li <fangchen.li@outlook.com>
2022-11-29 09:17:54 -0600

    CLN: remove test command

--- setup.py
+++ setup.py
@@ -1,27 +1,6 @@
 #!/usr/bin/env python
 
-import sys
-
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-
-class PyTestCommand(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 
 setup(
@@ -63,7 +42,4 @@ setup(
         ]
     },
     test_suite='tests',
-    cmdclass={
-        'test': PyTestCommand
-    }
 )
